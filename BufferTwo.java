fba6bb5d6b2090e22e88474455545b9adf5294f6
Chen Wei <s3499257@student.rmit.edu.au>
2017-05-28 17:53:05 +1000

    Redo it

--- BufferTwo.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-import java.util.*;
-
-public class BufferTwo {
-
-	/*Take care of reading and writing into memory from disk, page size is 8192*/
-	
-	 public static final int PAGE_SIZE = 8192;
-}
--- PageId.java
+++
@@ -1,4 +0,0 @@
-
-public interface PageId {
-	public int pageNumber();
-}
